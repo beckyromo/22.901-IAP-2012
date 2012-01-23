d9169b5ec1dbfdd06a58e07acb9a77b1db2029aa
Becky <romo@mit.edu>
2012-01-23 16:11:34 -0500

    code is finished

--- zeta.f90
+++ zeta.f90
@@ -1,26 +1,58 @@
 program zeta
 implicit none
-
   ! define variables
+  real :: x         ! input varialbe
+  real :: zeta_var  ! result
+  real :: zeta_fun  ! zeta function
+  
 
+  ! ask for user input
+  print *,'Enter x:'
+  read *,x
+
+  ! call subroutine to get zeta
+  call zeta_sub(x, zeta_var)
+
+  ! print result of subroutine
+  print *,'Sub::For x=',x,' , zeta is:',zeta_var
+
+  ! print result of function
+  print *,'Fun::For x=',x,' , zeta is:',zeta_fun(x)
+  
+  ! terminate program
+  stop
 
 end program zeta
 
 
 
 
-subroutine zeta_sub()
+subroutine zeta_sub(x,zeta_var)
 implicit none
+  ! formal variables
+  real,intent(in)  :: x         ! input varialbe
+  real,intent(out) :: zeta_var  ! result
+  ! local variables
+  real,parameter   :: pi=3.1415926  ! the constant pi
 
+  ! compute zeta
+  zeta_var=pi/x
 
 end subroutine zeta_sub
 
 
 
 
-function zeta_fun()
+function zeta_fun(x)
 implicit none
-  ! define variables
+  ! function name
   real :: zeta_fun
+  ! formal variables
+  real :: x         ! input varialbe
+  ! local variables
+  real,parameter   :: pi=3.1415926  ! the constant pi
+
+  ! compute result
+  zeta_fun=pi/x
 
 end function zeta_fun
