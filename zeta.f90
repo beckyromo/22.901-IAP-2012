75bc85846ef947c9ed4ee408d38a2157b86f3f4e
Becky <romo@mit.edu>
2012-01-23 15:46:34 -0500

    initial commit of zeta

---
+++ zeta.f90
@@ -0,0 +1,26 @@
+program zeta
+implicit none
+
+  ! define variables
+
+
+end program zeta
+
+
+
+
+subroutine zeta_sub()
+implicit none
+
+
+end subroutine zeta_sub
+
+
+
+
+function zeta_fun()
+implicit none
+  ! define variables
+  real :: zeta_fun
+
+end function zeta_fun
